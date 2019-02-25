09a32aedf3401334fdbbc7b389d577585013962f
Jiang Fengming <jiangfengming87@gmail.com>
2019-02-25 20:21:12 +0800

    tmp

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # teleman-cache
 Teleman Server-Side Rendering middleware
+
+## Usage
+```js
+import Teleman from 'teleman'
+import cache from 'teleman-cache'
+
+const api = new Teleman({
+  urlPrefix: 'https://api.example.com'
+})
+
+api.use(cache({
+  mode: CURRENT_ENV, // server | client
+  variable: '__API_CACHE__',
+
+  cacheKeyFn(ctx) {
+    return ctx.url.pathname + ctx.url.search
+  },
+
+  tagFn(ctx) {
+    return ctx.cacheTag
+  },
+  
+  onServerCached() {
+    window.PAGE_READY = true  
+  },
+
+  onClientConsumed() {
+    mountAPPIntoDOM()
+  }
+}))
+```
---
+++ dist/telemanCache.mjs
@@ -0,0 +1,108 @@
+var index = (({
+  mode,
+  variable,
+  cacheKeyFn,
+  tagFn,
+  onServerCached,
+  onClientConsumed
+} = {}) => {
+  let cache, script;
+
+  if (mode === 'server') {
+    if (onClientConsumed) onClientConsumed();
+    cache = [];
+    script = document.createElement('script');
+    document.body.insertBefore(script, document.body.getElementsByTagName('script')[0] || null);
+    resetServerIdleTimer();
+  } else {
+    cache = window[variable];
+
+    if (onClientConsumed) {
+      if (!cache || !cache.length) {
+        onClientConsumed();
+        onClientConsumed = null;
+      } else {
+        resetClientIdleTimer();
+      }
+    }
+  }
+
+  let serverIdleTimer;
+
+  function resetServerIdleTimer() {
+    clearTimeout(serverIdleTimer);
+    serverIdleTimer = setTimeout(() => {
+      script.text = `var ${variable} = ${JSON.stringify(cache)}`;
+
+      if (onServerCached) {
+        onServerCached();
+        onServerCached = null;
+      }
+    }, 450);
+  }
+
+  let clientIdleTimer;
+
+  function resetClientIdleTimer() {
+    clearTimeout(clientIdleTimer);
+
+    if (onClientConsumed) {
+      clientIdleTimer = setTimeout(() => {
+        if (onClientConsumed) {
+          onClientConsumed();
+          onClientConsumed = null;
+        }
+      }, 450);
+    }
+  }
+
+  return (ctx, next) => {
+    if (!cache) return next();
+    const key = cacheKeyFn ? cacheKeyFn(ctx) : ctx.url;
+    const tag = tagFn ? tagFn(ctx) : '';
+    const hitIndex = cache.findIndex(item => item.key === key);
+    const hit = hitIndex === -1 ? null : cache[hitIndex];
+
+    if (mode === 'server') {
+      if (hit && hit.tag === tag) return hit.body;
+      clearTimeout(serverIdleTimer);
+      return next().then(body => {
+        cache.push({
+          key,
+          tag,
+          body
+        });
+        return body;
+      }).finally(() => {
+        resetServerIdleTimer();
+      });
+    } else {
+      resetClientIdleTimer();
+
+      if (!hit) {
+        return next().finally(() => resetClientIdleTimer());
+      }
+
+      cache.splice(hitIndex, 1);
+
+      if (!cache.length) {
+        cache = null;
+
+        if (onClientConsumed) {
+          clearTimeout(clientIdleTimer);
+          onClientConsumed();
+          onClientConsumed = null;
+        }
+      }
+
+      if (hit.tag === tag) {
+        return hit.body;
+      } else {
+        return next().finally(() => resetClientIdleTimer());
+      }
+    }
+  };
+});
+
+export default index;
+//# sourceMappingURL=telemanCache.mjs.map
