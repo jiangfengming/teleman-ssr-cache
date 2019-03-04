877ab07530371fdc0d176135f5988a950596d641
Jiang Fengming <jiangfengming87@gmail.com>
2019-03-04 17:33:49 +0800

    * build * edit readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# teleman-cache
+# teleman-ssr-cache
 Teleman Server-Side Rendering middleware
 
 ## Usage
@@ -17,22 +17,58 @@ api.use(cache())
 
 ```js
 cache({
+  // The global variable to store the cached response when prerendering on the server side. e.g.
+  // <script> var __SSR_CACHE__ = ... </script>
+  // It will be inserted before the first <script> of <body>
+  // In client mode, requests shouldn't be sent before page parsed the <script> tag.
+  // The default name is '__SSR_CACHE__'
   variable: '__SSR_CACHE__',
 
-  mode: CURRENT_ENV, // server | client
+  // Current environment
+  // 'server' means the page is prerendering on the server side,
+  // 'client' means the page is rendering on the client side (user's browser).
+  // If `mode` isn't set. It is guessed from `variable` option.
+  // If window[variable] is `undefined`, the mode is `server`.
+  // Otherwise, the mode is `client`
+  mode: 'server',
 
+  // Customize the cache key
+  // The default is `ctx.url.href`.
+  // On the server side, the cached response will be stored with this key.
+  // On the client side, the request will find the cached response by this key.
   cacheKeyGenerator(ctx) {
     return ctx.url.pathname + ctx.url.search
   },
 
+  // Customize the cache tag
+  // If a cached response is found by cache key, but the cache tag doesn't met,
+  // the cached response will not be used, and will be removed from the cache store.
+  // The main purpose of cache tag is to eliminate the 450ms delay of `onClientConsumed` callback in some circumstances.
+  // For example, An API will return different response when the user has logged in or not logged in,
+  // you can call teleman.get('/article', { id: 123 }, { cacheTag: userId })
+  // On the server side rendering, the cache tag is `undefined`
+  // On the client side, the cache tag is userId (logged in) or `undefined` (not logged in), so the cache will be used only
+  // when not logged in. But anyhow, the cached response will be removed from the cache store.
+  // So `onClientConsumed` can be called immediately when the cache store becomes empty.
+  // If you don't use cache tag, but append the userId onto the cache key. On the client side, when the request is sent
+  // with login state, the cached response will not be hit, so the cache store will never be empty,
+  // and `onClientConsumed` has to be called after 450ms idle.
   tagGenerator(ctx) {
     return ctx.cacheTag
   },
   
+  // On the server side, when there's no more requests in 450ms,
+  // the middleware will seal the cached responses and insert into <body>,
+  // then `onServerCached` will be called.
+  // You can use this callback to explicitly tell the server-side rendering engine the prerendering has complete.
   onServerCached() {
     window.PAGE_READY = true  
   },
 
+  // On the client side, when all cached responses have been consumed (cache store is empty),
+  // or there's no more requests in 450ms, the `onClientConsumed` callback will be called.
+  // On the server side, the function will be called immediately.
+  // If you use async components, you can mount the app into DOM in this callback to prevent flash problem.
   onClientConsumed() {
     mountAPPIntoDOM()
   }
---
+++ dist/telemanSSRCache.js
@@ -0,0 +1,124 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global = global || self, global.telemanSSRCache = factory());
+}(this, function () { 'use strict';
+
+  var index = (function (_temp) {
+    var _ref = _temp === void 0 ? {} : _temp,
+        _ref$variable = _ref.variable,
+        variable = _ref$variable === void 0 ? '__SSR_CACHE__' : _ref$variable,
+        _ref$mode = _ref.mode,
+        mode = _ref$mode === void 0 ? window[variable] ? 'client' : 'server' : _ref$mode,
+        cacheKeyGenerator = _ref.cacheKeyGenerator,
+        tagGenerator = _ref.tagGenerator,
+        onServerCached = _ref.onServerCached,
+        onClientConsumed = _ref.onClientConsumed;
+
+    var cache, script, serverIdleTimer, clientIdleTimer;
+
+    if (mode === 'server') {
+      if (onClientConsumed) onClientConsumed();
+      cache = [];
+      script = document.createElement('script');
+      document.body.insertBefore(script, document.body.getElementsByTagName('script')[0] || null);
+      resetServerIdleTimer();
+    } else {
+      cache = window[variable];
+
+      if (onClientConsumed) {
+        if (!cache || !cache.length) {
+          onClientConsumed();
+          onClientConsumed = null;
+        } else {
+          resetClientIdleTimer();
+        }
+      }
+    }
+
+    function resetServerIdleTimer() {
+      clearTimeout(serverIdleTimer);
+      serverIdleTimer = setTimeout(function () {
+        script.text = "var " + variable + " = " + JSON.stringify(cache);
+
+        if (onServerCached) {
+          onServerCached();
+          onServerCached = null;
+        }
+      }, 450);
+    }
+
+    function resetClientIdleTimer() {
+      clearTimeout(clientIdleTimer);
+
+      if (onClientConsumed) {
+        clientIdleTimer = setTimeout(function () {
+          if (onClientConsumed) {
+            onClientConsumed();
+            onClientConsumed = null;
+          }
+        }, 450);
+      }
+    }
+
+    return function (ctx, next) {
+      if (!ctx.options.method || ctx.options.method.toUpperCase() !== 'GET' || !cache) {
+        return next();
+      }
+
+      var key = cacheKeyGenerator ? cacheKeyGenerator(ctx) : ctx.url;
+      var tag = tagGenerator ? tagGenerator(ctx) : '';
+      var hitIndex = cache.findIndex(function (item) {
+        return item.key === key;
+      });
+      var hit = hitIndex === -1 ? null : cache[hitIndex];
+
+      if (mode === 'server') {
+        if (hit && hit.tag === tag) return hit.body;
+        clearTimeout(serverIdleTimer);
+        return next().then(function (body) {
+          cache.push({
+            key: key,
+            tag: tag,
+            body: body
+          });
+          return body;
+        }).finally(function () {
+          resetServerIdleTimer();
+        });
+      } else {
+        resetClientIdleTimer();
+
+        if (!hit) {
+          return next().finally(function () {
+            return resetClientIdleTimer();
+          });
+        }
+
+        cache.splice(hitIndex, 1);
+
+        if (!cache.length) {
+          cache = null;
+
+          if (onClientConsumed) {
+            clearTimeout(clientIdleTimer);
+            onClientConsumed();
+            onClientConsumed = null;
+          }
+        }
+
+        if (hit.tag === tag) {
+          return hit.body;
+        } else {
+          return next().finally(function () {
+            return resetClientIdleTimer();
+          });
+        }
+      }
+    };
+  });
+
+  return index;
+
+}));
+//# sourceMappingURL=telemanSSRCache.js.map
